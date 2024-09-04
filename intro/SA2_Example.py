450ff70a7c8225ea041ee7d3a936073cb450bf14
dfriedman-tabor <dfriedman@taboracademy.org>
2024-09-04 15:26:54 -0400

    Add files via upload

---
+++ intro/SA2_Example.py
@@ -0,0 +1,22 @@
+
+
+answer = int(input("how old are you? "))
+
+if answer >= 16:
+    print("you can drive!")
+else:
+    print("you're too young to drive")
+
+
+answer = input("what is the local football team? ")
+
+if answer == "Patriots" or answer == "New England":
+    print("correct")
+else:
+    print("nope")
+
+
+
+
+
+
