3f776cccd96a583a29753529f2e4de7d90b7d877
dfriedman-tabor <dfriedman@taboracademy.org>
2024-09-05 11:45:37 -0400

    Add files via upload

--- intro/SA2_Example.py
+++ intro/SA2_Example.py
@@ -1,21 +1,43 @@
 
+# example for number 1
+age = int(input("how old are you? "))
 
-answer = int(input("how old are you? "))
-
-if answer >= 16:
-    print("you can drive!")
+if age >= 16:
+    print("you can drive")
 else:
-    print("you're too young to drive")
+    print("no car for you")
 
 
-answer = input("what is the local football team? ")
+# example for number 2
+winningTeam = input("who will win the super bowl? ")
 
-if answer == "Patriots" or answer == "New England":
-    print("correct")
+if winningTeam == "Eagles" or winningTeam == "Philadelphia":
+    print("of course")
 else:
     print("nope")
 
 
+# number 4
+number1 = int(input("enter a number "))
+number2 = int(input("enter another number "))
+
+if number1 > number2:
+    print("the biggest number is " + str(number1))
+else:
+    print("the biggest number is " + str(number2))
+
+
+# example for number 6
+location = input("which of the tri towns are you from? ")
+
+if location == "marion":
+    print("good choice")
+elif location == "rochester":
+    print("milk your cow")
+elif location == "wareham":
+    print("sketchy")
+else:
+    print("matty p")
 
 
 
